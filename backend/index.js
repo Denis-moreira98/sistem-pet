3e360e6b3c107c50731a0b801259541c9df4b8c1
Denis Moreira <silvad281@gmail.com>
2024-04-15 15:47:26 -0300

    feat: criando rota para cadastro de usuário

--- backend/index.js
+++ backend/index.js
@@ -1,19 +1,24 @@
 const express = require("express");
 const cors = require("cors");
+const dotenv = require("dotenv");
 
 const app = express();
+
+// DOTENV
+dotenv.config();
 
 // config JSON response
 app.use(express.json());
 
 // solve CORS request
-app.use(cors({ credentials: true, origin: "http://localhost:3000" }));
+app.use(cors({ credentials: true, origin: process.env.BASE_URL }));
 
 // Public folder for images
 app.use(express.static("public"));
 
 //  Routes
+const UserRoutes = require("./routes/UserRoutes");
+
+app.use("/users", UserRoutes);
 
 app.listen(5000, () => console.log("SERVE ONLINE!"));
-
-// teste
