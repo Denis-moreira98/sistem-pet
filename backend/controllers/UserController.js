6db52e5a63ff3b7d49d8c9e9c4b233b2c76b04f2
Denis Moreira <silvad281@gmail.com>
2024-04-17 13:26:59 -0300

    feat: buscando usuário por id

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -92,7 +92,6 @@ module.exports = class UserController {
       }
 
       //check if password match with db password
-
       const checkpassword = await bcrypt.compare(password, user.password);
 
       if (!checkpassword) {
@@ -120,5 +119,18 @@ module.exports = class UserController {
       }
 
       res.status(200).send(currentUser);
+   }
+
+   static async getUserById(req, res) {
+      const id = req.params.id;
+      const user = await User.findById(id).select("-password");
+
+      if (!user) {
+         res.status(422).json({ message: "Usuário não encontrado" });
+
+         return;
+      }
+
+      res.status(200).json({ user });
    }
 };
