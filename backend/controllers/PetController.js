f826bcfd9936d8eb34eb2c25577714107c31d75a
Denis Moreira <silvad281@gmail.com>
2024-04-19 14:00:09 -0300

    feat: agendando visita ao pet

--- backend/controllers/PetController.js
+++ backend/controllers/PetController.js
@@ -234,4 +234,46 @@ module.exports = class PetController {
          res.status(422).json({ message: err.message });
       }
    }
+
+   static async schedule(req, res) {
+      const id = req.params.id;
+
+      // check if pet exists
+      const pet = await Pet.findOne({ _id: id });
+
+      if (!pet) {
+         res.status(404).json({ message: "Pet não encontrado!" });
+      }
+
+      // check if user registered the pet
+      const token = getToken(req);
+      const user = await getUserByToken(token);
+
+      if (pet.user._id.equals(user._id)) {
+         res.status(422).json({
+            message: "Você não pode agendar uma visita com seu próprio pet!",
+         });
+         return;
+      }
+      if (pet.adopter) {
+         if (pet.adopter._id.equals(user._id)) {
+            res.status(422).json({
+               message: "Você já agendou uma visita para este pet!",
+            });
+            return;
+         }
+      }
+
+      //add user to pet
+      pet.adopter = {
+         _id: user._id,
+         name: user.name,
+         image: user.image,
+      };
+
+      await Pet.findByIdAndUpdate(id, pet);
+      res.status(200).json({
+         message: `A visita foi agenda com sucesso, entre em contato com ${pet.user.name} pelo telefone ${pet.user.phone}`,
+      });
+   }
 };
