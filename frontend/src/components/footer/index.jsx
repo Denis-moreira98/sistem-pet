2738258c92560424de39ddf7542756f35178b8cb
Denis Moreira <silvad281@gmail.com>
2024-04-21 11:30:27 -0300

    feat: criando componente footer

---
+++ frontend/src/components/footer/index.jsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.css";
+
+export function Footer() {
+   return (
+      <footer className={styles.footer}>
+         <p>
+            <b>Get A Pet</b> &copy; 2024
+         </p>
+      </footer>
+   );
+}
