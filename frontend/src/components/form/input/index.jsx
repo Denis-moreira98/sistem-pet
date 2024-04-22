30d4001ee4da8bda62b846d266643ce0c23f11f1
Denis Moreira <silvad281@gmail.com>
2024-04-22 14:20:24 -0300

    feat: criando componente Input

---
+++ frontend/src/components/form/input/index.jsx
@@ -0,0 +1,27 @@
+/* eslint-disable react/prop-types */
+import styles from "./styles.module.css";
+
+export function Input({
+   type,
+   text,
+   name,
+   placeholder,
+   handlerOnChange,
+   value,
+   multiple,
+}) {
+   return (
+      <div className={styles.form_control}>
+         <label htmlFor={name}>{text}:</label>
+         <input
+            type={type}
+            name={name}
+            id={name}
+            placeholder={placeholder}
+            onChange={handlerOnChange}
+            value={value}
+            {...(multiple ? { multiple } : "")}
+         />
+      </div>
+   );
+}
