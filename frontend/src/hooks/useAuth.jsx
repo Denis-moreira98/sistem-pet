826d876bdcfa7c33ce23c9f8dac9788acc25bacc
Denis Moreira <silvad281@gmail.com>
2024-04-23 12:26:57 -0300

    feat: criando função de logout

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -43,5 +43,17 @@ export default function useAuth() {
       navigate("/");
    }
 
-   return { register, authenticated };
+   function logout() {
+      const msgText = "Logout realizado com sucesso!";
+      const msgType = "success";
+
+      setAuthenticated(false);
+      localStorage.removeItem("token");
+      api.defaults.headers.Authorization = undefined;
+      navigate("/");
+
+      setFlashMessage(msgText, msgType);
+   }
+
+   return { register, authenticated, logout };
 }
