24d22e536af232768308a59ad86d1df6578bbe15
Denis Moreira <silvad281@gmail.com>
2024-04-22 21:57:17 -0300

    feat: salvando o token do usuário no localStorage e redirecionando para home

--- frontend/src/hooks/useAuth.jsx
+++ frontend/src/hooks/useAuth.jsx
@@ -1,11 +1,21 @@
 import api from "../utils/api";
-
-// import { useState, useEffect } from "react";
-// import { useHistory } from "reat-router-dom";
+import { useState, useEffect } from "react";
 import useFlashMessage from "./useFlashMessage";
+import { useNavigate } from "react-router-dom";
 
 export default function useAuth() {
+   const [authenticated, setAuthenticated] = useState(false);
    const { setFlashMessage } = useFlashMessage();
+   const navigate = useNavigate();
+
+   useEffect(() => {
+      const token = localStorage.getItem("token");
+
+      if (token) {
+         api.defaults.headers.Authorization = `Bearer ${JSON.parse(token)}`;
+         setAuthenticated(true);
+      }
+   }, []);
 
    async function register(user) {
       let msgText = "Cadastro realizado com sucesso!";
@@ -18,14 +28,20 @@ export default function useAuth() {
                return response.data;
             });
 
-         console.log(data);
+         authUser(data);
       } catch (error) {
-         console.log(error);
          msgText = error.response.data.message;
          msgType = "error";
       }
       setFlashMessage(msgText, msgType);
    }
 
-   return { register };
+   async function authUser(data) {
+      setAuthenticated(true);
+
+      localStorage.setItem("token", JSON.stringify(data.token));
+      navigate("/");
+   }
+
+   return { register, authenticated };
 }
