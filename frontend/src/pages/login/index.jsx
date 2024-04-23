0f2e54d0ebee1f1c4e5a21d73a22b5675d4d1344
Denis Moreira <silvad281@gmail.com>
2024-04-23 13:04:44 -0300

    feat: criando função de login e logando usuário

--- frontend/src/pages/login/index.jsx
+++ frontend/src/pages/login/index.jsx
@@ -1,37 +1,39 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import { Input } from "../../components/form/input";
 import { Link } from "react-router-dom";
 import styles from "../register/styles.module.css";
+import { Context } from "../../context/UserContext";
 
 export function Login() {
    const [user, setUser] = useState({});
+   const { login } = useContext(Context);
 
    function handleChange(e) {
       setUser({ ...user, [e.target.name]: e.target.value });
    }
 
-   // const handleSubmit = (e) => {
-   //    e.preventDefault();
-   //    login(user);
-   // };
+   const handleSubmit = (e) => {
+      e.preventDefault();
+      login(user);
+   };
 
    return (
       <section className={styles.form_container}>
          <h1>Login</h1>
-         <form>
+         <form onSubmit={handleSubmit}>
             <Input
-               text="E-mail"
+               text="Email"
                type="email"
                name="email"
-               placeholder="Digite o e-mail"
-               handleOnChange={handleChange}
+               placeholder="Digite o seu email"
+               handlerOnChange={handleChange}
             />
             <Input
                text="Senha"
                type="password"
                name="password"
-               placeholder="Digite a senha"
-               handleOnChange={handleChange}
+               placeholder="Digite a sua senha"
+               handlerOnChange={handleChange}
             />
             <input type="submit" value="Entrar" />
          </form>
